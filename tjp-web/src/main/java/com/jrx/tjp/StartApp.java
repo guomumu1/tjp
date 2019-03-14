9a163237ecf3f0de74c5ebe4d3918773acabb844
lin.guo <lin.guo@jiangrongxin.com>
2019-03-14 17:10:14 +0800

    分支一在窗口一上第二次修改

--- tjp-web/src/main/java/com/jrx/tjp/StartApp.java
+++ tjp-web/src/main/java/com/jrx/tjp/StartApp.java
@@ -16,5 +16,6 @@ public class StartApp {
         System.out.println("分支一添加的");
         System.out.println("分支一第二次添加");
         System.out.println("第一个窗口在分支一上修改");
+        System.out.println("分支一在窗口一上第二次修改");
     }
 }
